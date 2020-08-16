0d810f7599853c20bc12952d95c44c81c347dc43
teffshiro <teffshiro@gmail.com>
2020-08-16 10:36:54 -0400

    Initial athlete routes

--- models/athlete.js
+++ models/athlete.js
@@ -3,18 +3,19 @@
 const mongoose = require("mongoose");
 
 const athleteSchema = new mongoose.Schema({
+  id: {
+    type: Object,
+    required: true,
+  },
+
   name: {
     type: String,
     required: true,
   },
-  //   games: {
-  //     type: Array,
-  //     required: true,
-  //   },
-  //   era: {
-  //     type: String,
-  //     required: true,
-  //   },
+  year: {
+    type: Array,
+    required: true,
+  },
 });
 
 module.exports = mongoose.model("Athlete", athleteSchema);
